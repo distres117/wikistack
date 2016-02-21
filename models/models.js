4b3d52ef976698fca4969aa858280893004c67a3
distres117 <omcrobbie@gmail.com>
2016-02-21 09:11:38 -0500

    updated models

--- models/models.js
+++ models/models.js
@@ -7,18 +7,20 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongodb connection error:'));
 
 var pageSchema = new Schema({
-  title: String,
-  urlTitle: String,
-  content: String,
+  title: {type: String, required: true},
+  urlTitle: {type: String, required: true},
+  content: {type: String, required: true},
   date: {type: Date, default: Date.now},
-  status: Boolean,
-  author: String
+  status: String,
+  author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
 });
 
 var userSchema = new Schema({
-  name: String,
-  email: String
+  name: {type: String, required: true},
+  email: {type: String, required: true}
 });
 
 var Page = mongoose.model('Page', pageSchema);
 var User = mongoose.model('User', userSchema);
+
+moule.exports = {Page, User};
